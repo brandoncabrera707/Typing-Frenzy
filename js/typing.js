bcd532a95779c78a18a362717cb728a187f1645e
brandoncabrera707 <brandoncabrera707@gmail.com>
2024-07-30 10:02:37 -0700

    worked on tracking correct values inputted

--- js/typing.js
+++ js/typing.js
@@ -3,64 +3,55 @@ window.addEventListener('load', function() {
   passageText.textContent = 'Choco is crazy boy, Choco is good boy.';
   const passageWords = passageText.textContent.split(' ');
   const passageCharacters = passageText.textContent.split('')
-  console.log(passageWords);
-  //console.log(passageCharacters);
+
+
 
   const inputBox = document.querySelector('.typingBox');
   const userPassage = document.querySelector('.displayUserInput');
 
 
-  let correctValues = 0;
+  let numCorrectValues = 0;
   let wrongValues = 0;
   let wordsPerMinute = 0;
   let passageIndex =0;
   let storedWords = [];
+  let timerStarted = false;
+  let seconds = passageCharacters.length * 0.7;// fixed rate depending on passage length
 
 inputBox.addEventListener('input', function handleKeyTyping(event) {
   const inputText=inputBox.value.split('')
   const inputWords = inputBox.value.split(' ');
-  console.log(storedWords);
-  //console.log(inputText);
-  console.log(inputWords);
-  console.log(userPassage.textContent);
-for (let i = 0; i < inputWords.length ; i++){
-  if (inputWords[i] === passageWords[passageIndex]){
-    storedWords.push(inputWords[i]);
-    inputBox.value = '';
-    passageIndex++
-    break;
-  }
-}
-
-if (storedWords.length === passageWords.length && storedWords.join(' ') === passageWords.join(' ')){
-  inputBox.disabled = true; 
-}
-
-
-  for (let i = 0; i < passageText.length; i++) {
-      if (inputText[i] === passageCharacters[i]) {
-        correctValues++;
-        console.log( `here is the number of correct values: ${correctValues}`);
-      } else {
-        wrongValues++
-        break;
-      }
+  for (let i = 0; i < inputWords.length ; i++){
+    if (inputWords[i] === passageWords[passageIndex]){
+      storedWords.push(inputWords[i]);
+      inputBox.value = '';
+      numCorrectValues += passageWords[passageIndex].length;
+      console.log(numCorrectValues);
+      passageIndex++
+      break;
     }
-      /*let timerStarted = false;
-      let seconds = passageCharacters.length * 0.7;// fixed rate depending on passage length
+  
+}
+
+  if (storedWords.length === passageWords.length && storedWords.join(' ') === passageWords.join(' ')){
+    inputBox.disabled = true; 
+    seconds = 0;
+    console.log(numCorrectValues);
+  }      
       let secondspassed = 0;
       let interval;
+      
       if (!timerStarted) {
-  
       timerStarted = true;
       interval = setInterval(function() {
         if (seconds > 0) {
           seconds--;
           let secondsGiven = passageCharacters.length * 0.7;  
           secondspassed = secondsGiven - seconds;
-          console.log(`Seconds remaining: ${seconds}`);
+          console.log(`Seconds remaining: ${seconds.toFixed(0)}`);
           let elapsedTimeInMinutes = secondspassed / 60;
-          let wordsPerMinute = (correctValues / 5) / elapsedTimeInMinutes;// words on average are 5 chaacters in length
+          let wordsPerMinute = (numCorrectValues / 5) / elapsedTimeInMinutes;// words on average are 5 chaacters in length
+       
           } else {
             clearInterval(interval);
             console.log("Time's up!");
@@ -69,7 +60,7 @@ if (storedWords.length === passageWords.length && storedWords.join(' ') === pass
         }, 1000);
         
       }
-      */
+      
     
 
 
