5d533cb5425f3621addd7f9afae5936780ec0200
brandoncabrera707 <brandoncabrera707@gmail.com>
2024-08-15 11:33:38 -0700

    just something to go back to while making routes for api.js

--- api/api.js
+++ api/api.js
@@ -1,10 +1,13 @@
 
 const express = require('express');
 const fs = require('fs');
+const db = require('firebase/firebase.js');
+
 
 const app = express();
 
 app.use(express.json());
+
 
 const port = process.env.PORT || 1212;
 
@@ -12,23 +15,13 @@ app.listen(port,
   () =>  console.log(`it's alive on http://localhost:${port}`));
 
 
-app.get('/', (req,res) => {
+app.get('/db', (req,res) => {
   res.send('Hello World!!');
 });
 
 
+app.get('/passages', (req,res) => {
+
+})
 
 
-fs.readFile('passages.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
-  const jsonData = JSON.parse(data);
-
-  // Example: Accessing the 'author' and 'passage' keys from each object in an array
-  jsonData.forEach(item => {
-    console.log(`Author: ${item.author}`);
-    
-  });
-});
