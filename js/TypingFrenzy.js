a6e1cb9c357f4bcbcdd8f28b08e6bd388fcbe75a
brandoncabrera707 <brandoncabrera707@gmail.com>
2024-08-15 18:40:06 -0700

    fixed indents a bit

--- js/TypingFrenzy.js
+++ js/TypingFrenzy.js
@@ -1,7 +1,3 @@
-
-
-
-
 window.addEventListener('load', function() {
   let text;
   let author;
@@ -30,7 +26,6 @@ window.addEventListener('load', function() {
 
 
   const inputBox = document.querySelector('.typingBox');
-  const userPassage = document.querySelector('.displayUserInput');
 
 
 
@@ -53,72 +48,68 @@ inputBox.addEventListener('input', function handleKeyTyping(event) {
 if (storedWords.length === passageWords.length && storedWords.join(' ') === passageWords.join(' ')){
   inputBox.disabled = true; 
   seconds = 0;
-}      
-      
-
-  });
+  }      
+});
   
 
-  inputBox.addEventListener('keydown', function(event) {
+inputBox.addEventListener('keydown', function(event) {
  
 
-  const inputText = inputBox.value + event.key; // Simulate the input value after key press
-  const lastChar = inputText[inputText.length - 1]; // Get the last character typed
+const inputText = inputBox.value + event.key; // Simulate the input value after key press
+const lastChar = inputText[inputText.length - 1]; // Get the last character typed
  
-  if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock') {
+if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock') {
 
-    typedEntries++
-    console.log(`Total typed entries: ${typedEntries}`);
+  typedEntries++
+  console.log(`Total typed entries: ${typedEntries}`);
 
 
-    let secondsPassed = 0;
-    let wpmInterval;
+  let secondsPassed = 0;
+  let wpmInterval;
         
-    if (!timerStarted) {
-      timerStarted = true;
-      wpmInterval = setInterval(function() {
-        if (seconds > 0) {
-          seconds--;
-          timerDisplay.innerHTML = (`Seconds remaining: ${seconds.toFixed(0)}`);   
+  if (!timerStarted) {
+    timerStarted = true;
+    wpmInterval = setInterval(function() {
+      if (seconds > 0) {
+        seconds--;
+        timerDisplay.innerHTML = (`Seconds remaining: ${seconds.toFixed(0)}`);   
         
-          } else {
-            clearInterval(wpmInterval);
-            clearInterval(wpmInterval);
-            timerDisplay.innerHTML = ("Time's up!");
-            inputBox.disabled = true; // Disable the input box when the time is up
-          }
-  
-         
-       }, 1000);
+        } else {
+          clearInterval(wpmInterval);
+          clearInterval(wpmInterval);
+          timerDisplay.innerHTML = ("Time's up!");
+          inputBox.disabled = true; // Disable the input box when the time is up
+        }
+       
+      }, 1000);
     
           
         wpmInterval = setInterval(function() {
         let secondsGiven = passageCharacters.length * 0.7;  
-         secondsPassed = secondsGiven - seconds;
+        secondsPassed = secondsGiven - seconds;
         let elapsedTimeInMinutes = secondsPassed / 60;
         let wordsPerMinute = ((typedEntries / 5) - numWrongEntries) / elapsedTimeInMinutes;// this is the net WPM formula used by Speed Typing Online 
         if (wordsPerMinute > 0){
         wpmDisplay.innerHTML = (`WPM ${wordsPerMinute.toFixed(0)}`);
         }
-         }, 2500); // displays the wpm every 2.5 seconds on the page 
-       }
+      }, 2500); // displays the wpm every 2.5 seconds on the page 
+    }
         
       
-    //checks to see if the characters match 
-    if (lastChar === passageCharacters[passageIndex]) {
+      //checks to see if the characters match 
+      if (lastChar === passageCharacters[passageIndex]) {
       passageIndex++;
       numCorrectEntries++;
       console.log(`Correct entries: ${numCorrectEntries}`);
-  } else {
+      } 
+      else {
       numWrongEntries++;
       console.log(`Wrong entries: ${numWrongEntries}`);
   }
 
-  }
-
-
+}
     
-    if (event.key === ' ') { // Check if the space bar is pressed
+      if (event.key === ' ') { // Check if the space bar is pressed
       const inputWords = inputBox.value.trim().split(' '); // Get the input words and trim any extra spaces
   
       if (inputWords[inputWords.length - 1] === passageWords[passageWordIndex]) { // cleats the input box once the words match and user spaces to move to next word
