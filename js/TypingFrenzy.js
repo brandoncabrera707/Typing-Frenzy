4797f2b418d171525a45e373ec4a01bfb8f32e39
brandoncabrera707 <brandoncabrera707@gmail.com>
2024-08-27 12:23:58 -0700

    passage highlights correct chars green and wrong char red but messed up the checkForMatch so looking into that

--- js/TypingFrenzy.js
+++ js/TypingFrenzy.js
@@ -26,7 +26,9 @@ window.addEventListener('load', async function() {
   authorDisplay.textContent = (`By: ${author}`)
 
   const passageWords = passageText.textContent.split(' ');
+  console.log(passageWords)
   const passageCharacters = passageText.textContent.split('')
+
 
   const inputBox = document.querySelector('#typingBox');
 
@@ -37,7 +39,7 @@ window.addEventListener('load', async function() {
   let numCorrectEntries =0;
   let passageWordIndex = 0;
   let typedEntries = 0;
-  let isTrue = true; // trying to implment state machine...
+  let userHasMadeNoError = true; //state machine for when user input is right or wrong...
 
 
   const timerDisplay = document.querySelector('#timerDisplay');
@@ -51,72 +53,82 @@ inputBox.addEventListener('input', function handleKeyTyping(event) {
 
 inputBox.addEventListener('keydown', function(event) {
 
-if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock') { //any other value besides these three will be considered an entry
-
-  typedEntries++
-  console.log(`Total typed entries: ${typedEntries}`);
-  let secondsPassed = 0;
-  let wpmInterval;
-        
-  if (!timerStarted) {//will start timer count down once user starts inputting
-    timerStarted = true;
-    TimeInterval = setInterval(function() { //updates the time on the screen every second
-      if (time > 0) {
-        time--;
-        let  minutes = Math.floor(time/60);
-        let seconds = time - minutes * 60;
-        timerDisplay.innerHTML = (`${minutes}:${seconds.toFixed(0)}`);   
-
-        
-        
-      } 
-      else {
-        clearInterval(TimeInterval);  //Time will stop updating every seconds
-        clearInterval(wpmInterval);   //WPM will stop updating every 2.5 seconds
-        timerDisplay.innerHTML = ("Time's up!");
-        inputBox.disabled = true; // Disable the input box when the time is up
-      }
-       
-      }, 1000);
-    
-          
-    wpmInterval = setInterval(function() {
-      let secondsGiven = passageCharacters.length * 0.7;  
-      secondsPassed = secondsGiven - time;
-      let elapsedTimeInMinutes = secondsPassed / 60;
-      let wordsPerMinute = ((typedEntries / 5)) / elapsedTimeInMinutes;// this is the WPM formula used by Speed Typing Online 
-      if (wordsPerMinute > 0){
-      wpmDisplay.innerHTML = (`WPM: ${wordsPerMinute.toFixed(0)}`);
-     }
-    }, 2500); // displays the wpm every 2.5 seconds on the page 
-  }
-        
-
-      matchChar();
-
+if (userHasMadeNoError == false){
+  userHasMadeNoError = true
 }
-      ensureSpacing(event);
+else {
+  if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock' && userHasMadeNoError == true) { //any other value besides these three will be considered an entry
+
+    typedEntries++
+    //console.log(`Total typed entries: ${typedEntries}`);
+    let secondsPassed = 0;
+    let wpmInterval;
+          
+    if (!timerStarted) {//will start timer count down once user starts inputting
+      timerStarted = true;
+      TimeInterval = setInterval(function() { //updates the time on the screen every second
+        if (time > 0) {
+          time--;
+          let  minutes = Math.floor(time/60);
+          let seconds = time - minutes * 60;
+          timerDisplay.innerHTML = (`${minutes}:${seconds.toFixed(0)}`);   
+  
+          
+          
+        } 
+        else {
+          clearInterval(TimeInterval);  //Time will stop updating every seconds
+          clearInterval(wpmInterval);   //WPM will stop updating every 2.5 seconds
+          timerDisplay.innerHTML = ("Time's up!");
+          inputBox.disabled = true; // Disable the input box when the time is up
+        }
+         
+        }, 1000);
+      
+            
+      wpmInterval = setInterval(function() {
+        let secondsGiven = passageCharacters.length * 0.7;  
+        secondsPassed = secondsGiven - time;
+        let elapsedTimeInMinutes = secondsPassed / 60;
+        let wordsPerMinute = ((typedEntries / 5)) / elapsedTimeInMinutes;// this is the WPM formula used by Speed Typing Online 
+        if (wordsPerMinute > 0){
+        wpmDisplay.innerHTML = (`WPM: ${wordsPerMinute.toFixed(0)}`);
+       }
+      }, 2500); // displays the wpm every 2.5 seconds on the page 
+    }
+          
+  
+        matchChar();
+  
+  }
+}
+
+    ensureSpacing(event);
      
     //checks to see if last char of current input matches
     function matchChar(){
       const inputText = inputBox.value + event.key; // Simulate the input value after key press
-      console.log(inputText)
       const lastChar = inputText[inputText.length - 1]; // Get the last character typed
+      const trackCorrectChars = document.querySelectorAll('span')
       if (lastChar === passageCharacters[passageIndex]) {
         passageIndex++;
         for (i = 1; i <= passageIndex ; i++){
-          const trackCorrectChars = document.querySelectorAll('span')
           trackCorrectChars[i-1].style.color = "green" 
           inputBox.style.backgroundColor = "transparent";
+          trackCorrectChars[i-1].style.backgroundColor = "lightGreen"
    
         }
         numCorrectEntries++;
-        console.log(`Correct entries: ${numCorrectEntries}`);
+        //console.log(`Correct entries: ${numCorrectEntries}`);
         inputBox.style.backgroundColor = "transparent";
-       
         } 
         else {
-        inputBox.style.backgroundColor = "pink";
+        //trackCorrectChars[passageIndex].style.backgroundColor = "pink" 
+        inputBox.style.backgroundColor = "pink"   
+        userHasMadeNoError = false
+        setTimeout(() => {
+        event.preventDefault();
+        }, 50);
         }  
         
     }
@@ -128,6 +140,7 @@ if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock')
     
         if (inputWords[inputWords.length - 1] === passageWords[passageWordIndex]) { // clears the input box once the words match and user spaces to move to next word
           storedWords.push(inputWords[inputWords.length - 1]);
+          console.log(storedWords)
           inputBox.value = ''; // Clear the input box    
           passageWordIndex++;
         }
@@ -144,8 +157,7 @@ if (event.key != 'Backspace' && event.key != 'Shift' && event.key != 'CapsLock')
     // checks to see if the input is equal to the passage and if it is will disable the input box
     if (storedWords.length === passageWords.length && storedWords.join(' ') === passageWords.join(' ')){
       inputBox.disabled = true; 
-      inputBox.style.backgroundColor ="transparent"
-      seconds = 0;
+      time = 0;
     }    
 
   }
